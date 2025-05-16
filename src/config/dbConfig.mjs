494e8dd53b81b53c3b13840c92d26d8d27383244
Rocio-Abigail <abyruiz.rr@gmail.com>
2025-05-16 09:24:54 -0300

    agrego comentarios

--- src/config/dbConfig.mjs
+++ src/config/dbConfig.mjs
@@ -1,11 +1,19 @@
+// Importamos el módulo 'mongoose' para interactuar con MongoDB desde Node.js
 import mongoose from 'mongoose';
 
+// Definimos una función asíncrona que se encargará de conectar nuestra aplicación a la base de datos
 export async function connectDB() {
     try {
+        // Intentamos conectarnos a MongoDB usando una URI de conexión con credenciales y nombre de clúster
         await mongoose.connect('mongodb+srv://Grupo-13:grupo13@cursadanodejs.ls9ii.mongodb.net/Node-js');
+
+        // Si la conexión fue exitosa, se muestra este mensaje en consola
         console.log('Conexión exitosa a MongoDB');
     } catch (error) {
+        // Si ocurre un error durante la conexión, lo mostramos en consola
         console.error('Error al conectar a MongoDB:', error);
+
+        // Finalizamos el proceso con un código de error (1) para indicar que algo salió mal
         process.exit(1);
     }
 }
